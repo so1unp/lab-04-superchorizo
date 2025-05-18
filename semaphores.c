bd099fcf4a74cc57cc24dabb8665f756fd9fb4fb
nicolas <nicoezequiel16romero@gmail.com>
2025-05-18 14:56:14 -0300

    a

--- semaphores.c
+++ semaphores.c
@@ -44,9 +44,9 @@ int main(int argc, char *argv[])
 
         int valor = atoi(argv[3]);
 
-        sem_t *semaforo = sem_open(argv[2], O_CREAT, 0644, valor);
+        sem_t *sem_o = sem_open(argv[2], O_CREAT, 0644, valor);
 
-        if (semaforo == SEM_FAILED)
+        if (sem_o == SEM_FAILED)
         {
             perror("sem_open");
             exit(EXIT_FAILURE);
@@ -54,16 +54,14 @@ int main(int argc, char *argv[])
 
         printf("Semáforo '%s' creado con valor inicial %d\n", argv[2], valor);
 
-        if (sem_close(semaforo) == -1)
+        if (sem_close(sem_o) == -1)
         {
             perror("sem_close");
         }
         break;
-
     case 'u':
 
-        if (argc < 3)
-        {
+        if (argc < 3) {
             fprintf(stderr, "Uso: %s -u <nombre_semaforo>\n", argv[0]);
             exit(EXIT_FAILURE);
         }
@@ -71,121 +69,92 @@ int main(int argc, char *argv[])
         // abre el semaforo si y solo si existe.
         sem_t *sem_u = sem_open(argv[2], 0);
 
-        if (sem_u == SEM_FAILED)
-        {
+        if (sem_u == SEM_FAILED) {
             perror("sem_open");
             exit(EXIT_FAILURE);
         }
-
         // aca se realizaria el UP
-        if (sem_post(sem_u) != -1)
-        {
+        if (sem_post(sem_u) != -1) {
             printf("Se realizó UP sobre el semáforo '%s'\n", argv[2]);
-        }
-        else
-        {
+        } else {
             perror("sem_post");
         }
 
-        if (sem_close(sem_u) == -1)
-        {
+        if (sem_close(sem_u) == -1) {
             perror("sem_close");
         }
         break;
     case 'd':
-        if (argc < 3)
-        {
+
+        if (argc < 3) {
             fprintf(stderr, "Uso: %s -d <nombre_semaforo>\n", argv[0]);
             exit(EXIT_FAILURE);
         }
-
         // abre el semaforo si y solo si existe.
         sem_t *sem_d = sem_open(argv[2], 0);
 
-        if (sem_d == SEM_FAILED)
-        {
+        if (sem_d == SEM_FAILED) {
             perror("sem_open");
             exit(EXIT_FAILURE);
         }
 
-        // se realiza un DOWN
-        if (sem_wait(sem_d) != -1)
-        {
+        // se realiza DOWN
+        if (sem_wait(sem_d) != -1) {
             printf("Se realizó DOWN sobre el semáforo '%s'\n", argv[2]);
-        }
-        else
-        {
+        } else {
             perror("sem_wait");
         }
 
-        if (sem_close(sem_d) == -1)
-        {
+        if (sem_close(sem_d) == -1) {
             perror("sem_close");
         }
         break;
     case 'b':
 
-        if (argc < 3)
-        {
+        if (argc < 3) {
             fprintf(stderr, "Uso: %s -b <nombre_semaforo>\n", argv[0]);
             exit(EXIT_FAILURE);
         }
-
         // abre el semaforo si y solo si existe.
         sem_t *sem = sem_open(argv[2], 0);
-        if (sem == SEM_FAILED)
-        {
+        if (sem == SEM_FAILED) {
             perror("sem_open");
             exit(EXIT_FAILURE);
         }
-
         // Se elimina el semaforo
-        if (sem_unlink(argv[2]) != -1)
-        {
+        if (sem_unlink(argv[2]) != -1) {
             printf("Semáforo '%s' eliminado\n", argv[2]);
-        }
-        else
-        {
+        } else {
             perror("sem_unlink");
         }
 
-        if (sem_close(sem) == -1)
-        {
+        if (sem_close(sem) == -1) {
             perror("sem_close");
         }
         break;
     case 'i':
 
-        if (argc < 3)
-        {
+        if (argc < 3) {
             fprintf(stderr, "Uso: %s -i <nombre_semaforo>\n", argv[0]);
             exit(EXIT_FAILURE);
         }
-
         // abre el semaforo si y solo si existe.
         sem_t *sem_i = sem_open(argv[2], 0);
-        if (sem_i == SEM_FAILED)
-        {
+        if (sem_i == SEM_FAILED) {
             perror("sem_open");
             exit(EXIT_FAILURE);
         }
-
         // aca te dice la informacion actual del semaforo
         int valor_actual;
-        if (sem_getvalue(sem_i, &valor_actual) != -1)
-        {
+        if (sem_getvalue(sem_i, &valor_actual) != -1) {
             printf("El valor actual del semáforo '%s' es: %d\n", argv[2], valor_actual);
-        }
-        else
-        {
+        } else{
             perror("sem_getvalue");
         }
 
-        if (sem_close(sem_i) == -1)
-        {
+        if (sem_close(sem_i) == -1) {
             perror("sem_close");
         }
-
         break;
     case 'h':
         usage(argv);
